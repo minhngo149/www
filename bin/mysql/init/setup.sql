5c038a4cc876b885b51efa953f257b993d47c2ee
mido <minh.ngo@gumiviet.com>
2021-12-27 14:29:21 +0700

    refactor(db): upgrade script sql

--- bin/mysql/init/setup.sql
+++ bin/mysql/init/setup.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS docker_local_db;
 
-CREATE USER 'docker_local_db_user'@'localhost' IDENTIFIED BY 'docker_local_db_pw';
-GRANT ALL PRIVILEGES ON docker_local_db.* TO 'docker_local_db_user'@'%';
+CREATE USER 'docker_local_db_user'@'%' IDENTIFIED BY 'docker_local_db_pw';
+GRANT ALL PRIVILEGES ON *.* TO 'docker_local_db_user'@'%';
 FLUSH PRIVILEGES;
